e34073b3a1ede6b2b4a69dad182a4c2d470d0fde
Martin Krutzfeld <mkrutzfeld.ext@it4ipm.de>
2020-02-17 22:41:58 +0100

    etwas lernen

--- src/app/uebung/uebung-eins.ts
+++ src/app/uebung/uebung-eins.ts
@@ -1,3 +1,21 @@
+/**
+ * Lieber Wissbegieriger,
+ *
+ * hier ist die Funktion die du verändern musst, damit das Beispiel funktioniert.
+ * Du solltest am besten ansonsten nichts veränden, da das Beispiel dann nicht mehr funktioniert.
+ *
+ * Du siehst hier eine Funktion namens "uebeungEins"
+ * Sie hat drei Parameter:
+ *  - faktor1
+ *  - faktor2
+ *  - eingabeProdukt
+ *
+ *  Sie gibt einen Wert zurück, der hinter "return" steht.
+ *  Was wird dort aktuell berechnet?
+ *  (1*2) wird zuerst ausgewertet, deshalb steht dort immer 2==2.
+ *  Das ergibt den Wahrheitswert 'wahr'.
+ *  Was musst du hier ersetzen damit die Funktion auf unser Beispiel passt?
+ */
 export function uebungEins(faktor1, faktor2, eingabeProdukt) {
-    return 1+1 == 2 ;
+    return (1*2) == 2 ;
 }
