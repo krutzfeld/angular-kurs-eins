2a41a802196f5142283b8d0e946ff7afc0ef9cf7
Martin Krutzfeld <mkrutzfeld.ext@it4ipm.de>
2020-08-30 12:42:22 +0200

    build repair

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,14 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './../../../aws-exports';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styles: []
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
 })
-export class AppComponent implements OnInit {
-    ngOnInit(): void {
-      Amplify.configure(awsconfig);
-    }
+export class AppComponent {
 }
