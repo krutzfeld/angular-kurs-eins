9f79a73aa1dbab588387777b83edf979189587b5
Martin Krutzfeld <mkrutzfeld.ext@it4ipm.de>
2020-08-30 14:17:20 +0200

    tutorial stuff

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,7 +61,10 @@ import '@angular/localize/init';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-(window as any).global = window
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
